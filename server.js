f4abe0f8521c189c367fb65b964ae85bcdb02d44
marlon9604 <umscaa@hotmail.com>
2021-03-02 14:00:26 +0000

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-  res.send('Hello World 2.0');
-=======
 app.get('/', function (req, res) {
   res.send('Hello World 2.0!');
 });
 const port = process.env.port || 8080;
 app.listen(port, function () {
   console.log('myapp listening on port ' + port);
->>>>>>> origin
 });
-
