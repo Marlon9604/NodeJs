c17c9bfbf8fac1d1451b81499a8cf2d24603e678
Marlon9604 <79286293+Marlon9604@users.noreply.github.com>
2021-03-01 21:28:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require(‘express’);
+const express = require('express');
 const app = express();
-app.get(‘/’, function (req, res) {
-  res.send(‘Hello World!’);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 const port = process.env.port || 8080;
 app.listen(port, function () {
-  console.log(‘myapp listening on port ‘ + port);
+  console.log('myapp listening on port ' + port);
 });
 
