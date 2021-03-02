e82093ffcf0ea5ea5a55f71aa127d540e32c51f0
marlon9604 <umscaa@hotmail.com>
2021-03-02 15:44:21 +0000

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World 2.0!');
+  res.send('Hello World 2.0 prueba docker imagen!');
 });
 const port = process.env.port || 8080;
 app.listen(port, function () {
